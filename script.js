0c7658c55c3723bcbe391b5af1ab4299bfedde55
aingianni92 <ingianni.anthony@gmail.com>
2022-09-10 11:09:49 -0400

    Added some space to the fretboard

--- script.js
+++ script.js
@@ -16,3 +16,9 @@ guitarNoteBtns.forEach(btn => {
         audio.play();
     });
 });
+
+// I have a working guitar. What do I need to create an interactive game based around these notes.
+// Create two classes that represent the players health and the computers health.
+// A successful note match will result in the computers health going down, wheres a miss will knock down the players health.
+// First round the notes are visible on the buttons.
+// Second round the notes will dissapear from the innertext of the button.
