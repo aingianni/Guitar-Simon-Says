4799f5ff583520ee53ac6bf653e7d344e25bd0ee
aingianni92 <ingianni.anthony@gmail.com>
2022-09-10 17:48:02 -0400

    Had id's where i should have had classes, fixed and changed

--- script.js
+++ script.js
@@ -21,12 +21,6 @@ guitarNoteBtns.forEach(btn => {
     });
 });
 
-// I have a working guitar. What do I need to create an interactive game based around these notes.
-// Create two classes that represent the players health and the computers health.
-// A successful note match will result in the computers health going down, wheres a miss will knock down the players health.
-// First round the notes are visible on the buttons.
-// Second round the notes will dissapear from the innertext of the button.
-
 // Gloabal variables for compare.
 let playerChoice = null;
 let compChoice = null;
