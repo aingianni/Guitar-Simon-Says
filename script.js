9ee3b1c79ce6db4174e17396a028553d16ebd80f
aingianni92 <ingianni.anthony@gmail.com>
2022-09-11 13:12:07 -0400

    Added tutorial modals for the start of the game

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const compChoiceBtn = document.getElementById('comp-choice-btn');
 const submitBtn = document.getElementById('submit');
 const playerHealth = document.getElementById('player-health');
 const compHealth = document.getElementById('comp-health');
+const tutPlayerHealth = document.getElementById('player-health-tut');
+const tutCompHealth = document.getElementById('comp-health-tut');
+const tutFretboard = document.getElementById('fretboard-tut');
+const tutCompNote = document.getElementById('comp-note-tut');
+const modal = document.getElementById('modal');
 
 const guitarNotesLayout = [
     ['E4', 'F4', 'Gb4', 'G4', 'Ab4', 'A4', 'Bb4', 'B4', 'C5', 'Db5', 'D5', 'Eb5', 'E5'],
@@ -83,4 +88,49 @@ const compareChoices = () => {
 submitBtn.addEventListener('click', (evt) => {
     compareChoices();
     render();
+})
+
+// Tutorial modals, these will run at the start of the page and indivually be clicked through.
+
+const openTut = () => {
+    modal.style.display = 'block';
+    tutFretboard.style.visibility = 'visible';
+    document.getElementById('fretboard').style.zIndex = '1';
+    document.getElementById('fretboard').style.animation = 'pulse 2s infinite';
+}
+
+setTimeout(openTut, 500);
+
+tutFretboard.addEventListener('click', () => {
+    document.getElementById('fretboard').style.zIndex = '0';
+    document.getElementById('fretboard').style.animation = '';
+    tutFretboard.style.visibility = 'hidden';
+    tutPlayerHealth.style.visibility = 'visible';
+    document.getElementById('player-stats').style.zIndex = '1';
+    document.getElementById('player-stats').style.animation = 'pulse 2s infinite';
+})
+
+tutPlayerHealth.addEventListener('click', () => {
+    document.getElementById('player-stats').style.zIndex = '0';
+    document.getElementById('player-stats').style.animation = '';
+    tutPlayerHealth.style.visibility = 'hidden';
+    tutCompHealth.style.visibility = 'visible';
+    document.getElementById('comp-health').style.zIndex = '1';
+    document.getElementById('comp-health').style.animation = 'pulse 2s infinite';
+})
+
+tutCompHealth.addEventListener('click', () => {
+    document.getElementById('comp-health').style.zIndex = '0';
+    document.getElementById('comp-health').style.animation = '';
+    tutCompHealth.style.visibility = 'hidden';
+    tutCompNote.style.visibility = 'visible';
+    document.getElementById('display-content').style.zIndex = '1';
+    document.getElementById('display-content').style.animation = 'pulse 2s infinite';
+})
+
+tutCompNote.addEventListener('click', () => {
+    document.getElementById('display-content').style.zIndex = '0';
+    document.getElementById('display-content').style.animation = '';
+    modal.style.display = 'none';
+    tutCompNote.style.visibility = 'hidden';
 })
